e7af9e42fca333b745fb59a794ea5568356fc6a1
saurabh pathak <saurabhpathak072@gmail.com>
2021-08-08 14:22:21 +0530

    added react markdown editor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+/** @format */
+
+import { useState } from 'react';
 import './App.css';
+import ReactMarkDown from 'react-markdown';
 
 function App() {
+  const [markDown, setMarkDown] = useState('## Markdown Preview');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markDown}
+          onChange={(e) => setMarkDown(e.target.value)}
+          id='markdown'
+        />
+        <article className='result'>
+          <ReactMarkDown>{markDown}</ReactMarkDown>
+        </article>
+      </section>
+    </main>
   );
 }
 
